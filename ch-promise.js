b8869bc2d3aa3dfa9bc7ed87484aa721a2ac72cd
NeuroSpeech <ackava@gmail.com>
2015-04-05 10:32:53 +0000

    added promise.js

---
+++ ch-promise.js
@@ -0,0 +1,65 @@
+(function(g){
+  
+  function chPromise(self,f){
+    this.self = self;
+    this.list = [];
+    this.thenQ = [];
+    this.failQ = [];
+    f.apply(self,this);
+  }
+  
+  chPromise.prototype = {
+    add: function(p){
+      var px = { promise: p, state: ''};
+      this.list.add(px);
+      var self = this;
+      p.then(function(r){
+        px.result = r;
+        px.state = 'done';
+        self.finish();
+      }).fail(function(r){
+        px.fail = r;
+        px.state = 'fail';
+        self.finish();
+      });
+    },
+    finish: function(){
+      var list = this.list;
+      var n = list.length;
+      var failed = false;
+      var item;
+      var r = [this];
+      for(var i=0;i<n;i++){
+        item = list[i];
+        if(item.state == 'fail'){
+          failed = true;
+          break;
+        }
+        if(!item.state){
+          return;
+        }
+        r.push(item.result);
+      }
+      
+      list = failed ? this.failQ : this.thenQ;
+      n = list.length;
+      var self = this.self;
+      for (var i = 0; i < n; i++) {
+        item = list[i];
+        item.apply(self,r);
+      }        
+    }
+    ,
+    then: function(f){
+      this.thenQ.push(f);
+    },
+    fail: function(f){
+      this.failQ.push(f);
+    }
+  };
+  
+  function $ps(self,f){
+    return new chPromise(self,f);
+  }
+  g.$ps = $ps;
+})(this);
