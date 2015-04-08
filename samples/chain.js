1697521713827fb45f1a43c0fca029bb2848461c
NeuroSpeech <ackava@gmail.com>
2015-04-08 18:45:38 +0000

    work in progress

--- samples/chain.js
+++ samples/chain.js
@@ -1,4 +1,4 @@
-var $p, $ps, $pif, $ploop, $pswitch;
+var $p, $ps, $pif, $ploop, $pswitch, promiseResult;
 
 function chain(){
   var a = this.modify(
@@ -9,12 +9,51 @@ function chain(){
 
 function chainResult(){
   var a = null;
-  $ps(this,function(p){
-    p.add($.get('/a'));
-    p.add($.get('/b'));
-  })
-  .then(function(p,r1,r2){
-    a = this.modify(this.process(r1), r2);
-    console.log(a);
-  });
+  return promiseResult(this,[
+    { 
+      push:[
+        function(){
+          return $.get('/a');
+        },function(){
+          return $.get('/b');
+        }
+      ]
+    },
+    function(r1,r2){
+      a = this.modify(this.process(r1,r2));
+      console.log(a);
+    }
+  ]);
+}
+
+function complexChain(){
+  var a = this.modify(
+      this.process( $.get('/a') ? $.get('/b') : $.get('/c') )
+    );
+}
+
+function complexChainResult(){
+  var a;
+  return promiseResult(this,[
+    {
+      push: {
+        "if": {
+          async: function(){
+            return $.get('/a');
+          }
+        },
+        "then": {
+          async: function(){
+            return $.get('/b');
+          }
+        },
+        "else": function(){
+          return $.get('/c');
+        }
+      }
+    },
+    function(r1){
+      a = this.modify(this.process(r1));
+    }
+  ]);
 }
